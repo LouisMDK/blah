9c7a4d8bc69e151131c2f8b3d053df6269e6b6f1
Louis PAINTER <E211731S@univ-nantes.fr>
2021-12-14 11:07:58 +0100

    ajout NewSystem

--- src/particles/new.go
+++ src/particles/new.go
@@ -1,6 +1,10 @@
 package particles
 
-import "project-particles/config"
+import (
+	"project-particles/config"
+	"math/rand"
+	"log"
+)
 
 // NewSystem est une fonction qui initialise un système de particules et le
 // retourne à la fonction principale du projet, qui se chargera de l'afficher.
@@ -8,13 +12,32 @@ import "project-particles/config"
 // Dans sa version actuelle, cette fonction affiche une particule blanche au
 // centre de l'écran.
 func NewSystem() System {
-	return System{Content: []Particle{
-		Particle{
-			PositionX: float64(config.General.WindowSizeX) / 2,
-			PositionY: float64(config.General.WindowSizeY) / 2,
-			ScaleX:    1, ScaleY: 1,
-			ColorRed: 1, ColorGreen: 1, ColorBlue: 1,
-			Opacity: 1,
-		},
-	}}
+
+
+	var particules []Particle
+	
+	var getPosition func() (float64, float64)
+
+	// si la variable RandomSpawn dans le fichier config est True, la fonction getPosition() renvoie des valeurs aléatoires
+	// sinon elle renvoie les valeurs issues de SpawnX et SpawnY dans le fichier de config
+	if config.General.RandomSpawn {
+		getPosition = func() (float64, float64) {
+			return float64(rand.Intn(config.General.WindowSizeX)), float64(rand.Intn(config.General.WindowSizeY))
+		}
+	}else {
+		getPosition = func() (float64, float64) {
+			return float64(config.General.SpawnX), float64(config.General.SpawnY)
+		}
+	}
+		
+	for i  := 0; i < config.General.InitNumParticles; i++ {
+		var x, y = getPosition()
+		var p Particle = Particle{PositionX: x, PositionY: y}
+		particules = append(particules, p)
+	}
+
+	if config.General.Debug {
+		log.Println("Nouveau système créé avec pour contenu :", particules)
+	}
+	return System{Content: particules}
 }
